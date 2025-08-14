32045eb97dc0372dbfa69f8489068940c23a64d9
Enrique Farias <Enrique.Farias@isco-pipe.com>
2025-08-14 15:50:56 -0500

    reports directory path issue resolved

--- src/report_generator.py
+++ src/report_generator.py
@@ -34,7 +34,7 @@ class ReportGenerator:
     def __init__(self, db_manager, config_manager):
         self.db_manager = db_manager
         self.config_manager = config_manager
-        self.reports_dir = "reports"
+        self.reports_dir = os.path.join(os.getcwd(), "reports")
         self._ensure_reports_directory()
         self.report_counter = self._load_report_counter()
         
